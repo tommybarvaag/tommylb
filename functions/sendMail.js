280bfcf9a5c2613477866ce8d51d74600b209723
Tommy Barvåg <tommy@barvaag.com>
2019-10-31 17:48:02 +0100

    Fix send mail lambda to use promise

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,17 +1,26 @@
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   const sendGridMail = require("@sendgrid/mail");
+
   sendGridMail.setApiKey(process.env.NEXT_APP_SEND_GRID_SEND_MAIL_API_KEY);
 
-  sendGridMail.send({
-    to: "tommybarvaag@gmail.com",
-    from: "tommybarvaag@gmail.com",
-    subject: "Test email from netlify functions",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-  });
-
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+  sendGridMail
+    .send({
+      to: "tommybarvaag@gmail.com",
+      from: "tommybarvaag@gmail.com",
+      subject: "Test email from netlify functions",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+    })
+    .then(response => {
+      callback(null, {
+        statusCode: response.statusCode,
+        body: response.message
+      });
+    })
+    .catch(error => {
+      callback(null, {
+        statusCode: error.code,
+        body: error.message
+      });
+    });
 };
