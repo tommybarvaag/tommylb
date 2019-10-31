412278e273967a022bf99c755dcb2926ee14d2f3
Tommy Barvåg <tommy@barvaag.com>
2019-10-31 20:13:15 +0100

    Rewrite send mail lamda

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,26 +1,43 @@
-exports.handler = async function(event, context, callback) {
-  const sendGridMail = require("@sendgrid/mail");
+const sendgridMail = require("@sendgrid/mail");
 
-  sendGridMail.setApiKey(process.env.NEXT_APP_SEND_GRID_SEND_MAIL_API_KEY);
+export async function handler(event, context) {
+  const apiKey = process.env.NEXT_APP_SEND_GRID_SEND_MAIL_API_KEY;
 
-  sendGridMail
-    .send({
-      to: "tommybarvaag@gmail.com",
-      from: "tommybarvaag@gmail.com",
-      subject: "Test email from netlify functions",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-    })
-    .then(response => {
-      callback(null, {
-        statusCode: response.statusCode,
-        body: response.message
-      });
-    })
-    .catch(error => {
-      callback(null, {
-        statusCode: error.code,
-        body: error.message
-      });
+  if (!apiKey) {
+    return { statusCode: 500, body: "Missing SendGrid API key" };
+  }
+
+  try {
+    const body = JSON.parse(event.body);
+
+    if (!body || !body.email || !body.name || !body.text) {
+      return {
+        statusCode: 400,
+        body: "Bad request"
+      };
+    }
+
+    sendgridMail.setApiKey(process.env.NEXT_APP_SEND_GRID_SEND_MAIL_API_KEY);
+
+    await sendgridMail.send({
+      to: "tommy@barvaag.com",
+      from: "post@tommylb.com",
+      subject: `Contact from ${body.name}`,
+      html: `<div>
+          <h3>New message from ${body.name}</h3>
+          <p>Can be contacted at: <a href="mailto:${body.email}">${body.email}</a>.</p>
+          <p>${body.text}</p>
+        </div>`
     });
-};
+
+    return {
+      statusCode: 202,
+      body: "Accepted"
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Error"
+    };
+  }
+}
