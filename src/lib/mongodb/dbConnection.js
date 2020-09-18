b96784aea33ab2b790ee02c1cb7265c76f24a6de
Tommy Barvåg <tommy@barvaag.com>
2020-09-18 16:22:31 +0200

    Add useUnifiedTopology to mongo db client

--- src/lib/mongodb/dbConnection.js
+++ src/lib/mongodb/dbConnection.js
@@ -2,5 +2,6 @@ import { MongoClient } from "mongodb";
 
 export const getClient = async () =>
   await MongoClient.connect(process.env.TLB_MONGODB_STRAVA_CONNECTION_STRING, {
+    useUnifiedTopology: true,
     useNewUrlParser: true
   });
