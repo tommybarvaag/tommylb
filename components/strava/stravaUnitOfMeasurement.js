cc79156f9cd37351bf3fd31254e89cae878e0f21
Tommy Barvåg <tommy@barvaag.com>
2021-03-30 19:34:38 +0200

    Adjust margins

--- components/strava/stravaUnitOfMeasurement.js
+++ components/strava/stravaUnitOfMeasurement.js
@@ -3,7 +3,7 @@ import Text from "../text";
 
 export default function StravaUnitOfMeasurement({ title, value, ...other }) {
   return (
-    <div className="flex flex-col justify-center align-center mb-2 mr-3" {...other}>
+    <div className="flex flex-col justify-center align-center mr-6" {...other}>
       <Text className="text-sm">{title}</Text>
       <Text className="text-lg font-bold">{value}</Text>
     </div>
