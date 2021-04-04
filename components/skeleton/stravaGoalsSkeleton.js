5a8a6a9ec3571ef154c8dc98a6c13a7edd658384
Tommy Barvåg <tommy@barvaag.com>
2021-04-04 20:32:19 +0200

    Update class tag typo after copy pasting tailwind example

--- components/skeleton/stravaGoalsSkeleton.js
+++ components/skeleton/stravaGoalsSkeleton.js
@@ -3,15 +3,15 @@ import * as React from "react";
 export default function StravaGoalsSkeleton({ ...other }) {
   return (
     <div className="w-full mb-12" {...other}>
-      <div class="w-full">
-        <div class="h-full overflow-hidden">
-          <div class="pt-2">
-            <h2 class="bg-gray-400 animate-pulse h-8 w-2/3 mb-3"></h2>
-            <p class="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-transparent"></p>
-            <p class="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
-            <p class="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
-            <p class="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
-            <p class="leading-relaxed mb-5 w-full h-3 animate-pulse bg-gray-400"></p>
+      <div className="w-full">
+        <div className="h-full overflow-hidden">
+          <div className="pt-2">
+            <h2 className="bg-gray-400 animate-pulse h-8 w-2/3 mb-3"></h2>
+            <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-transparent"></p>
+            <p className="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
+            <p className="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
+            <p className="leading-relaxed mb-6 w-full h-3 animate-pulse bg-gray-400"></p>
+            <p className="leading-relaxed mb-5 w-full h-3 animate-pulse bg-gray-400"></p>
           </div>
         </div>
       </div>
