e9d4accfa1afd83ae560180638a02a395d2feead
Tommy Barvåg <tommy@barvaag.com>
2021-12-08 20:54:05 +0100

    Upgrade packages to latest

--- components/strava/stravaNameAndValue.tsx
+++ components/strava/stravaNameAndValue.tsx
@@ -8,11 +8,13 @@ type StravaNameAndValueProps = {
 
 export default function StravaNameAndValue({ name, value, ...other }: StravaNameAndValueProps) {
   return (
-    <div className="flex" {...other}>
-      <Text as="div" className="mr-2 flex-grow">
+    <div className="flex gap-3" {...other}>
+      <Text as="div" className="flex-grow max-w-[220px]">
         {name}
       </Text>
-      <Text as="div">{value}</Text>
+      <Text as="div" className="text-right ml-auto">
+        {value}
+      </Text>
     </div>
   );
 }
