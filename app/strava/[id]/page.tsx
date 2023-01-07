8a826f2bb0c3051bdef43d95030f325e032a9c99
Tommy Barvåg <tommy@barvaag.com>
2023-01-07 22:19:44 +0100

    remove force dynamic

--- app/strava/[id]/page.tsx
+++ app/strava/[id]/page.tsx
@@ -9,7 +9,6 @@ import { Prisma, StravaActivity } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 export const runtime = "edge";
-export const dynamic = "force-dynamic";
 
 interface StravaActivityPageProps {
   params: {
