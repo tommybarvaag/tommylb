926d438bfcb45426b46c8e4be38d0252441b15e3
Tommy Barvåg <tommy@barvaag.com>
2023-11-01 22:20:41 +0100

    Fix mdx component source

--- app/(cv)/layout.tsx
+++ app/(cv)/layout.tsx
@@ -52,9 +52,9 @@ export default function CurriculumVitaeLayout({ children }: { children: React.Re
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.8"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.8"
                     d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
                   ></path>
                 </svg>
