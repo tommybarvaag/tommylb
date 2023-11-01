84ab8d573032c74bd6ce1d1779ba4d9c38ed372e
Tommy Barvåg <tommy@barvaag.com>
2023-11-01 22:21:32 +0100

    Fix redirect

--- app/(cv)/cv/route.ts
+++ app/(cv)/cv/route.ts
@@ -5,5 +5,5 @@ import { permanentRedirect } from "next/navigation";
 export const runtime: ServerRuntime = "edge";
 
 export async function GET(request: Request) {
-  return permanentRedirect(`${getAbsoluteUrl()}/cv/about-tommy`);
+  return permanentRedirect(`${getAbsoluteUrl()}/cv/about`);
 }
