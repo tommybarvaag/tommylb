bb9d963d724b44492f89f3b5987d13e917133d5f
Tommy Barvåg <tommy@barvaag.com>
2023-12-04 22:02:18 +0100

    Edge runtime for intercepted routes

--- app/@modal/(.)connect/page.tsx
+++ app/@modal/(.)connect/page.tsx
@@ -2,6 +2,8 @@ import { ShowPlatform } from "@/app/(cv)/cv/_components/show-platform";
 import ConnectDialog from "@/app/@modal/_components/connect-dialog";
 import ConnectDialogVaul from "@/app/@modal/_components/connect-vaul";
 
+export const runtime = "experimental-edge";
+
 export default function ConnectDialogPage() {
   return (
     <ShowPlatform
