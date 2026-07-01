1fe535eb4025d212952ca20c327293dd0250dc2d
Tommy Barvåg <tommy@barvaag.com>
2026-07-01 19:44:15 +0200

    chore: delete strava routes, api, components and lib

--- app/api/strava/import/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  // await strava.importActivities();
-  return NextResponse.json({ status: "Imported" });
-}
